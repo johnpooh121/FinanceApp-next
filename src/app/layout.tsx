a3aec21e4dcbe723bcf8a0fb1c5f6822ff0ef3ff
johnpooh121 <johnpooh121@gmail.com>
2025-05-24 16:01:22 +0900

    fix : case, folder name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import Footer from "@/conmponents/footer";
+import Footer from "@/components/footer";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
